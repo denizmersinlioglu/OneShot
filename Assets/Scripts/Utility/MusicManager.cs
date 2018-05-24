00c80c0930adc41322b98fa24a42aa4b8f946135
Deniz <denizm@valensas.com>
2018-05-25 00:53:07 +0300

    ProjectSettings: Music Manager Deleted

--- Assets/Scripts/Utility/MusicManager.cs
+++ Assets/Scripts/Utility/MusicManager.cs
@@ -8,28 +8,18 @@ public class MusicManager : MonoBehaviour {
 	
 	public AudioSource music;
 	
+	private void OnEnable() {
+
+	}
 	void Awake(){
-		DontDestroyOnLoad(gameObject);
+		// DontDestroyOnLoad(gameObject);
 	}
 	
 	void Start () {
-		
-		music = this.GetComponent<AudioSource>();
-		music.clip= LevelMusic[0];
-		music.Play();
-	}
 	
-	void OnLevelWasLoaded(int level){
-		//music.volume = PlayerPrefControl.GetMasterVolume();
-		if(LevelMusic[level] && SceneManager.GetActiveScene().buildIndex != 0){
-		music.clip = LevelMusic[level];
-		if(level==4){music.loop=false;}
-		else music.loop = true;
-		music.Play();
-	}
 	}
 	
 	public void ChangeVolume(float volume){
-		music.volume = volume;
+		
 	}
 }
