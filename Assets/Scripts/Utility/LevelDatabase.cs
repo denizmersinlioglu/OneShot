37a32d51474dc65e2dd9e708430e7e158761a8df
denizmersinlioglu <dmersinlioglu13@ku.edu.tr>
2018-05-29 01:03:13 +0300

    Git ignore added

---
+++ Assets/Scripts/Utility/LevelDatabase.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LevelDatabase : ScriptableObject
+{
+    public List<Level> levelDatabase = new List<Level>();
+}
+ 
+[Serializable]
+public class Level {
+
+	public int number;
+	public LevelControlType controlType;
+	public LevelStatus status;
+	
+	public Level(int number, LevelControlType type, LevelStatus status) {
+		this.number = number;
+		this.controlType = type;
+		this.status = status;
+	}
+
+	public Level(){
+		number = 0;
+		controlType = LevelControlType.none;
+		status = LevelStatus.none;
+	}
+}
+
+public enum LevelStatus{
+	none = -3,
+	locked = -1,
+	incompleted = 0,
+	completeOneStar = 1,
+	completeTwoStars = 2,
+	completeThreeStars = 3,
+}
+
+public enum LevelControlType{
+	acc, launch, none
+}
+ 
+
+
