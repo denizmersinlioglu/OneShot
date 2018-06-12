873378aa0baff67a5ca8064c7127703c774b9c3a
Deniz Mersínlioglu <mersinliogludeniz@gmail.com>
2018-06-12 23:09:24 +0300

    AppIconUpdate

--- Assets/Scripts/SceneControllers/GameController.cs
+++ Assets/Scripts/SceneControllers/GameController.cs
@@ -5,18 +5,18 @@ namespace SceneControllers
 	public class GameController : MonoBehaviour {
 	
 		/*  
-		Level Builder has already created the game environment according to current level.
-	 	The aim of this class is control the game state according to existing level structure, control types and active levels win/lose statements.
+			Level Builder has already created the game environment according to current level.
+	 		The aim of this class is control the game state according to existing level structure, control types and active levels win/lose statements.
 	
-		You may try to use update function to control state of the game.
-		However game is based on hit counts. Therefore you need to use colliderEnter2D callbacks to control the game state.
-		Need to create a notification - observer structure to get these callbacks from active levels ball.
+			You may try to use update function to control state of the game.
+			However game is based on hit counts. Therefore you need to use colliderEnter2D callbacks to control the game state.
+			Need to create a notification - observer structure to get these callbacks from active levels ball.
 
-	 	Just access to Level Builders active level to get these data from Level Database.
-	 	Get hit count from level instance - Control win/lose state of the level.
-     	Get success limit numbers from level instance - Control user success rate according to twoStarsHit# and threeStarsHit#.
-    	When user complete the level - Update LevelDatabase asset according to users success.
- 	*/
+	 		Just access to Level Builders active level to get these data from Level Database.
+	 		Get hit count from level instance - Control win/lose state of the level.
+     		Get success limit numbers from level instance - Control user success rate according to twoStarsHit# and threeStarsHit#.
+    		When user complete the level - Update LevelDatabase asset according to users success.
+ 		*/
 
 		// Use this for initialization
 		private void Start () {
